9e5920f1e802ac8d9039ea3b8d476aaf44bd139b
Muhammad Sefat <muhammadsefat55@gmail.com>
2024-02-18 19:53:24 +0600

    complete full disign

---
+++ script.js
@@ -0,0 +1,8 @@
+const seats = document.getElementsByClassName('seat');
+
+for(let seat of seats){
+    seat.addEventListener('click',function(){
+        let seatNumber = parseInt(document.getElementById('seat-number').innerText);
+       
+    })
+}
