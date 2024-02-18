34ab77d96bfe6a1ef074f85b2a0ae384f8cfab0b
Muhammad Sefat <muhammadsefat55@gmail.com>
2024-02-18 21:49:39 +0600

    Decrease seat finish

--- script.js
+++ script.js
@@ -2,7 +2,24 @@ const seats = document.getElementsByClassName('seat');
 
 for(let seat of seats){
     seat.addEventListener('click',function(){
+
+        // decrease number
         let seatNumber = parseInt(document.getElementById('seat-number').innerText);
-       
+       let newSeatNumber = seatNumber - 1;
+       setNumberElementById('seat-number',newSeatNumber);
+
+    //    increase number
+    let conterNumber = parseInt(document.getElementById('counter-number').innerText);
+    console.log(conterNumber);
+    let newCountNumber = conterNumber + 1;
+    setNumberElementById('counter-number',newCountNumber);
+
+    // background color set
+        seat.style.background = "limeGreen";
     })
+}
+
+function setNumberElementById(elementId,value){
+    const element = document.getElementById(elementId);
+    element.innerText = value;
 }
