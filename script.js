bf1d08e6b92dc0f2b7181f8f9d5b93bbd5379c77
Muhammad Sefat <muhammadsefat55@gmail.com>
2024-02-19 07:23:47 +0600

    complete append section

--- script.js
+++ script.js
@@ -18,11 +18,37 @@ for(let seat of seats){
         seat.style.background = "limeGreen";
         seat.style.color = "white";
 
-     // update price
+
+
+     // update total price
         const perPrice = parseInt(document.getElementById('per-price').innerText);
         const totalPrice = parseInt(document.getElementById('total-price').innerText);
         let newTotalPrice = totalPrice + perPrice;
         setNumberElementById("total-price",newTotalPrice)
+
+
+     //update grand total price
+     const grandTotal = parseInt(document.getElementById('grand-total').innerText);
+     setNumberElementById('grand-total',newTotalPrice);
+
+
+
+    // append price
+        const appendDiv = document.getElementById("append-div");
+        const li = document.createElement('li');
+        li.style.display = 'flex';
+        li.style.justifyContent = "space-between";
+        const p1 = document.createElement('p');
+        const p2 = document.createElement('p');
+        const p3 = document.createElement('p');
+        p1.innerText = seat.innerText;
+        p2.innerText = "Economoy";
+        p3.innerText = perPrice;
+        li.appendChild(p1);
+        li.appendChild(p2);
+        li.appendChild(p3);
+        appendDiv.appendChild(li);
+
     })
 }
 
